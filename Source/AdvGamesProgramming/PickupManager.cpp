36dc509ce27bc3389bcf60dcf8860cf6b417b784
Kevin L <ReMashed@users.noreply.github.com>
2020-10-06 10:25:25 +1100

    MysteryBox Spawning, Material changes
    - MysteryBox now spawns at different height ranges of the map depending on its Type.
    -- Completed via estblashing height ranges, low, middle, high (Each are 33% of map in terms of height).
    -- Sort the Nodes by height via Bubble Sort Algorithm
    - Changed material for the MysteryBox Weapon type.

--- Source/AdvGamesProgramming/PickupManager.cpp
+++ Source/AdvGamesProgramming/PickupManager.cpp
@@ -78,8 +78,6 @@ void APickupManager::SpawnMysteryBoxPickup()
 	}
 }
 
-//TEMP FUNCTION -- USE IF NEEDED
-//Current Location Generation is fine. 
 FVector APickupManager::GenerateLocation()
 {
 	//Generate Location based on the following:
@@ -93,69 +91,48 @@ FVector APickupManager::GenerateLocation()
 	//This bit can be pcg
 	//Algorithm for finding highest node via Z
 	FVector NewSpawnLocation = FVector::ZeroVector; 
+	TArray<FVector> SortedNodesArray = SortNodesByZValue(PossibleSpawnLocations, false);
+
+	//Get a count for types of effects the Mytery Box has 
+	int32 NumberOfTypes = 0;
+	UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("MysteryBoxPickupType"), true);
+	if (EnumPtr)
+	{
+		//Minus as there's an additional Enum element added by unreal
+		NumberOfTypes = EnumPtr->NumEnums() - 1;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Number of Mystery Box Types, %i"), NumberOfTypes);
+
+	//Mystery Box will spawn at different heights of the map depending on its "Type"
+	//Weapon - Highest 1/3(33%) of points will contain weapons
+	//Boost Effects (Health, Speeed) - Lowest 1/3 of points will contain boost effects
+	//Middle Range will not contain any mystery boxes.
+	//Note: There are only 3 Types currently
+	//----- Varying number of "Types" will cause changes to its range
+	//----- These comments will refer to Mystery Boxes have only 3 types.
 	if (MysteryBox)
 	{
 		if (MysteryBox->Type == MysteryBoxPickupType::WEAPON)
 		{
 			UE_LOG(LogTemp, Warning, TEXT("WEAPON IS VALID: NOW GENERATING NEW LOCATION"));
-			//Find Highest Node
-			FVector HighestPoint = FVector::ZeroVector;
-			float HighestZValue = TNumericLimits<float>::Min();
-
-			for (FVector CurrentNode : PossibleSpawnLocations)
-			{
-				if (CurrentNode.Z > HighestZValue)
-				{
-					HighestZValue = CurrentNode.Z;
-					HighestPoint = CurrentNode;
-				}
-			}
-
-			//Z value Offset to avoid object touching spawning in ground 
-			HighestPoint.Z += 100.0f;
-			NewSpawnLocation = HighestPoint;
-
-
+			//Choose a random point within highest points of the map (Top 33%)
+			//Sort the Array asecndingly by z-value or height
+			
+			//Grab the Range of the High Points
+			//Starting value is the number of Nodes minus 1/3 of the number of nodes
+			//This will produce a number representing 66% of the Number of Nodes
+			int32 HighPointsStart = SortedNodesArray.Num() - (SortedNodesArray.Num() / NumberOfTypes); 
+			int32 HighPointsEnd = SortedNodesArray.Num();
+			int32 HighRangeIndex = FMath::FRandRange(HighPointsStart, HighPointsEnd);
+			
+			//Set new spawn within the high range
+			NewSpawnLocation = SortedNodesArray[HighRangeIndex];
+			//Offset height upwards to avoid collision with the ground
+			NewSpawnLocation.Z += 100.0f;
 		}
 		else
 		{
-			/*
-			FVector LowestPoint = FVector::ZeroVector;
-			float LowestZValue = TNumericLimits<float>::Max();
-
-			for (FVector CurrentNode : PossibleSpawnLocations)
-			{
-				//If current Node Height is less than Current Lowest Z value
-				if (CurrentNode.Z < LowestZValue)
-				{
-					LowestZValue = CurrentNode.Z;
-					LowestPoint = CurrentNode;
-				}
-			}
-			//Z value Offset to avoid object touching spawning in ground 
-			LowestPoint.Z += 100.0f;
-			UE_LOG(LogTemp, Warning, TEXT("EFFECTS SPAWNING AT LOWEST POINT"));
-			NewSpawnLocation = LowestPoint;
-			*/
-			/*
-			//Choose a Random location
-			//Offset the Height upwards to avoid Object touching the ground
-			int32 RandomIndex = FMath::RandRange(0, PossibleSpawnLocations.Num() - 1);
-			FVector SpawnLocation = PossibleSpawnLocations[RandomIndex];
-			SpawnLocation.Z += 100.0f;
-
-			return SpawnLocation;
-			*/
-			int32 EnumCount = 0;
-			UEnum* EnumPtr = FindObject<UEnum>(ANY_PACKAGE, TEXT("MysteryBoxPickupType"), true);
-			if(EnumPtr)
-			{
-				//Minus as there's an additional Enum element added by unreal
-				EnumCount = EnumPtr->NumEnums() - 1;
-			}
-			UE_LOG(LogTemp, Warning, TEXT("ENUM SIZE, %i"), EnumCount);
-
-			int32 LowPoints = PossibleSpawnLocations.Num() / EnumCount;
+			int32 LowPoints = PossibleSpawnLocations.Num() / NumberOfTypes;
 
 			//The Lowest 1/3 of the Sorted Array reflects the lowest points
 			//Now choose a point within that range to spawn the item
@@ -204,17 +181,39 @@ TArray<FVector> APickupManager::SortNodesByZValue(TArray<FVector> NodeLocations,
 	if (bIsDescendingOrder)
 	{
 		//Descending order sorting
+		//Bubble Sorting Algorithm
+		bool bSwappedHappened;
+		//Do executed at least once then while statement check occurs
+		do
+		{
+			bSwappedHappened = false;
+			//Loop through each node
+			//To avoid reach outside the bounds of the array
+			//we minus 1 from the comparison since we use i++
+			for (int32 i = 0; i < PossibleSpawnLocations.Num() - 1; i++)
+			{
+				//If current Node is lower than the next node
+				//Makes the larger node come first in the Array
+				//Swap position in the array 
+				if (SortedArray[i].Z < SortedArray[i + 1].Z)
+				{
+					Swap(SortedArray[i], SortedArray[i + 1]);
+					bSwappedHappened = true;
+				}
+			}
+		} while (bSwappedHappened);
 	}
-	else
+	else 
 	{
 		//Ascending order sorting
+		//Bubble Sorting Algorithm
 		bool bSwappedHappened;
 		do
 		{
 			bSwappedHappened = false;
 			//Loop through each node
 			//To avoid reach outside the bounds of the array
-			//we minus 1 from the comparison
+			//we minus 1 from the comparison since we use i++
 			for (int32 i = 0; i < PossibleSpawnLocations.Num() - 1; i++)
 			{
 				//If current Node is higher than the next node
