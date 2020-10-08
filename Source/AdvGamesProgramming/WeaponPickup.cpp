8511646c04ef0a610b43be5a30fe7aae3389b7a7
tuonglong <tuonglong200034@gmail.com>
2020-10-09 02:06:59 +1100

    Updated gun models

--- Source/AdvGamesProgramming/WeaponPickup.cpp
+++ Source/AdvGamesProgramming/WeaponPickup.cpp
@@ -13,39 +13,95 @@ void AWeaponPickup::OnGenerate()
 	//COMMON = 50% chance
 	UE_LOG(LogTemp, Warning, TEXT("GENERATING WEAPON PICKUP"))
 		int32 RandomRarityValue = FMath::RandRange(1, 100);
+	int32 RandomWeaponValue = FMath::RandRange(1, 4);
+
 	//Will populate the RandBoolArray with a shuffled set of boolean values depending on the weapon rarity.
+	switch (RandomWeaponValue)
+	{
+	case 1:
+		FiringType = WeaponFiringType::SINGLE_SHOT;
+		break;
+
+	case 2:
+		FiringType = WeaponFiringType::MULTI_SHOT;
+		break;
+
+	case 3:
+		FiringType = WeaponFiringType::AUTOMATIC;
+		break;
+
+	case 4:
+		FiringType = WeaponFiringType::SNIPER;
+		break;
+	}
+
 	TArray<bool> RandBoolArray;
 	if (RandomRarityValue <= 5)
 	{
 		Rarity = WeaponPickupRarity::LEGENDARY;
 		GenerateRandBooleanArray(5, 4, RandBoolArray);
-		FiringType = WeaponFiringType::SNIPER;
 	}
 	else if (RandomRarityValue <= 20)
 	{
 		Rarity = WeaponPickupRarity::MASTER;
 		GenerateRandBooleanArray(5, 3, RandBoolArray);
-		FiringType = WeaponFiringType::AUTOMATIC;
 	}
 	else if (RandomRarityValue <= 50)
 	{
 		Rarity = WeaponPickupRarity::RARE;
 		GenerateRandBooleanArray(5, 1, RandBoolArray);
-		FiringType = WeaponFiringType::MULTI_SHOT;
 	}
 	else
 	{
 		Rarity = WeaponPickupRarity::COMMON;
 		GenerateRandBooleanArray(5, 0, RandBoolArray);
-		FiringType = WeaponFiringType::SINGLE_SHOT;
+	}
+
+	switch(FiringType)
+	{
+	case WeaponFiringType::SINGLE_SHOT:
+
+		BulletDamage = (RandBoolArray[0] ? FMath::RandRange(15.0f, 20.0f) : FMath::RandRange(5.0f, 15.0f));
+		MuzzleVelocity = (RandBoolArray[1] ? FMath::RandRange(10000.0f, 20000.0f) : FMath::RandRange(5000.0f, 10000.0f));
+		MagazineSize = (RandBoolArray[2] ? FMath::RandRange(15, 25) : FMath::RandRange(5, 10));
+		WeaponAccuracy = (RandBoolArray[3] ? FMath::RandRange(300.0f, 1000.0f) : FMath::RandRange(50.0f, 200.0f));
+		FireRate = (RandBoolArray[4] ? FMath::RandRange(0.1f, 0.2f) : FMath::RandRange(0.2f, 0.4f));
+
+		break;
+
+	case WeaponFiringType::MULTI_SHOT:
+
+		BulletDamage = (RandBoolArray[0] ? FMath::RandRange(5.0f, 15.0f) : FMath::RandRange(2.0f, 10.0f));
+		MuzzleVelocity = (RandBoolArray[1] ? FMath::RandRange(10000.0f, 20000.0f) : FMath::RandRange(5000.0f, 10000.0f));
+		MagazineSize = (RandBoolArray[2] ? FMath::RandRange(20, 40) : FMath::RandRange(5, 20));
+		WeaponAccuracy = (RandBoolArray[3] ? FMath::RandRange(50.0f, 80.0f) : FMath::RandRange(10.0f, 30.0f));
+		FireRate = (RandBoolArray[4] ? FMath::RandRange(0.2f, 0.3f) : FMath::RandRange(0.4f, 0.5f));
+
+		break;
+
+	case WeaponFiringType::AUTOMATIC:
+
+		BulletDamage = (RandBoolArray[0] ? FMath::RandRange(5.0f, 10.0f) : FMath::RandRange(1.0f, 5.0f));
+		MuzzleVelocity = (RandBoolArray[1] ? FMath::RandRange(15000.0f, 20000.0f) : FMath::RandRange(10000.0f, 10000.0f));
+		MagazineSize = (RandBoolArray[2] ? FMath::RandRange(20, 40) : FMath::RandRange(20, 30));
+		WeaponAccuracy = (RandBoolArray[3] ? FMath::RandRange(50.0f, 100.0f) : FMath::RandRange(10.0f, 60.0f));
+		FireRate = (RandBoolArray[4] ? FMath::RandRange(0.08f, 0.2f) : FMath::RandRange(0.2f, 0.3f));
+
+		break;
+
+	case WeaponFiringType::SNIPER:
+
+		BulletDamage = (RandBoolArray[0] ? FMath::RandRange(50.0f, 100.0f) : FMath::RandRange(30.0f, 50.0f));
+		MuzzleVelocity = (RandBoolArray[1] ? FMath::RandRange(15000.0f, 20000.0f) : FMath::RandRange(10000.0f, 15000.0f));
+		MagazineSize = (RandBoolArray[2] ? FMath::RandRange(5, 10) : FMath::RandRange(1, 5));
+		WeaponAccuracy = (RandBoolArray[3] ? FMath::RandRange(800.0f, 1000.0f) : FMath::RandRange(500.0f, 800.0f));
+		FireRate = (RandBoolArray[4] ? FMath::RandRange(0.3f, 0.5f) : FMath::RandRange(0.6f, 0.8f));
+
+		break;
 	}
 
 	//Assign the good or bad weapon characteristics based on the result of the random boolean array.
-	BulletDamage = (RandBoolArray[0] ? FMath::RandRange(15.0f, 30.0f) : FMath::RandRange(2.0f, 15.0f));
-	MuzzleVelocity = (RandBoolArray[1] ? FMath::RandRange(10000.0f, 20000.0f) : FMath::RandRange(5000.0f, 10000.0f));
-	MagazineSize = (RandBoolArray[2] ? FMath::RandRange(20, 100) : FMath::RandRange(1, 20));
-	WeaponAccuracy = (RandBoolArray[3] ? FMath::RandRange(100.0f, 1000.0f) : FMath::RandRange(20.0f, 80.0f));
-	FireRate = (RandBoolArray[4] ? FMath::RandRange(0.1f, 0.2f) : FMath::RandRange(0.3f, 0.5f));
+
 }
 
 void AWeaponPickup::GenerateRandBooleanArray(int32 ArrayLength, int32 NumTrue, TArray<bool> & RandBoolArray)
