2f8171a3604ff87ab9c1a73deaaccb8e2f79b008
Kevin L <ReMashed@users.noreply.github.com>
2020-10-29 11:08:03 +1100

    Added Bonus Task from Week 11 lab
    - Need to fix Kill count and death count anchor points, doesn't show fully on bigger window sizes
    - Working subsystem that allows a client to host and one to join a game on a standalone game.

--- Source/AdvGamesProgramming/MainGameInstance.cpp
+++ Source/AdvGamesProgramming/MainGameInstance.cpp
@@ -5,7 +5,6 @@
 #include "OnlineSessionSettings.h"
 #include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
-#include "Interfaces/OnlineSessionInterface.h"
 
 
 UMainGameInstance::UMainGameInstance(const FObjectInitializer& ObjectInitialize)
@@ -27,41 +26,15 @@ void UMainGameInstance::Init()
 		SessionInterface = Subsystem->GetSessionInterface();
 
 		SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UMainGameInstance::OnCreateSessionComplete);
+		SessionInterface->OnDestroySessionCompleteDelegates.AddUObject(this, &UMainGameInstance::OnDestroySessionComplete);
+		SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UMainGameInstance::OnFindSessionComplete);
+		SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UMainGameInstance::OnJoinSessionComplete);
 	}
 	else
 	{
 		UE_LOG(LogTemp, Error, TEXT("Unable to find Online Subsystem"));
 	}
-
-	CreateSession(TEXT("Test Session"));
-}
-
-
-void UMainGameInstance::CreateSession(FName SessionName)
-{
-	if (SessionInterface.IsValid())
-	{
-		FOnlineSessionSettings SessionSettings;
-		SessionSettings.bIsLANMatch = true;
-		SessionSettings.NumPublicConnections = 3;
-		SessionSettings.bShouldAdvertise = true;
-		SessionInterface->CreateSession(0, SessionName, SessionSettings);
-
-	}
-}
-
-void UMainGameInstance::OnCreateSessionComplete(FName SessionName, bool bSuccess)
-{
-	if (bSuccess)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Session Created Successfully"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Session was not Created"));
-	}
-
-
+	//CreateSession(TEXT("Test Session"));
 }
 
 void UMainGameInstance::LoadMenu()
@@ -79,5 +52,131 @@ void UMainGameInstance::LoadMenu()
 		APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
 		PC->SetInputMode(InputMode);
 		PC->bShowMouseCursor = true;
+	}
+}
+
+
+void UMainGameInstance::CreateSession(FName SessionName)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Creating Session"));
+	if (SessionInterface.IsValid())
+	{
+		FOnlineSessionSettings SessionSettings;
+		SessionSettings.bIsLANMatch = true;
+		SessionSettings.NumPublicConnections = 3;
+		SessionSettings.bShouldAdvertise = true;
+
+		SessionInterface->CreateSession(0, SessionName, SessionSettings);
+	}
+}
+
+void UMainGameInstance::DestroySession(FName SessionName)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Destroying Session"));
+	if (SessionInterface.IsValid()) //If session exists
+	{
+		SessionInterface->DestroySession(SessionName);
+	}
+}
+
+void UMainGameInstance::FindSession()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Finding Sessions"));
+	SessionSearch = MakeShareable(new FOnlineSessionSearch());
+	if (SessionSearch.IsValid())
+	{
+		SessionSearch->bIsLanQuery = true;
+		SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+		//Delegate OnFindSessionsComplete will be triggered
+		//which is where joining the found session occurs
+	}
+}
+
+void UMainGameInstance::JoinSession()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Joining Session"));
+	if (SessionInterface.IsValid() && SessionSearch.IsValid())
+	{
+		//If there is a session in the list to join.
+		if (SessionSearch->SearchResults.Num() > 0)
+		{
+			//Could make the Session be of Client choice
+			//Maybe showcase possible sessions on the UI
+			SessionInterface->JoinSession(0, TEXT("Test Session"), SessionSearch->SearchResults[0]);
+		}
+	}
+}
+
+//Delegate function, called after CreateSession()
+//If session name already exists, bsuccess = false
+void UMainGameInstance::OnCreateSessionComplete(FName SessionName, bool bSuccess)
+{
+	if (bSuccess)
+	{
+		//Move client to multiplayer map
+		UWorld* World = GetWorld();
+		UE_LOG(LogTemp, Warning, TEXT("Session Created Successfully"));
+		UE_LOG(LogTemp, Warning, TEXT("Session Created Successfully: %s - Joining Session"), *SessionName.ToString());
+
+		if (World)
+		{
+			FInputModeGameOnly InputState;
+			GetFirstLocalPlayerController()->SetInputMode(InputState);
+			World->ServerTravel(TEXT("/Game/Levels/MultiplayerMap?listen"));
+		}
+		//FindSession();
+	}
+	else
+	{
+		//If CreateSession() caused bSucess to be false
+		//due to the sessionname already existing, delete that existing session
+		//Then create a new one when Destroying is complete
+		UE_LOG(LogTemp, Warning, TEXT("Session was not Created"));
+		DestroySession(SessionName);
+	}
+}
+
+void UMainGameInstance::OnDestroySessionComplete(FName SessionName, bool bSuccess)
+{
+	//If Session is destroyed 
+	if (bSuccess && SessionInterface.IsValid())
+	{
+		CreateSession(SessionName);
+	}
+}
+
+void UMainGameInstance::OnFindSessionComplete(bool bSuccess)
+{
+	if (bSuccess)
+	{
+		//Stores a array of possible sessions to join. 
+		TArray<FOnlineSessionSearchResult> SearchResults = SessionSearch->SearchResults;
+		UE_LOG(LogTemp, Warning, TEXT("Found %i Sessions"), SearchResults.Num())
+			for (const FOnlineSessionSearchResult& SearchResult : SearchResults)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Found Session: %s"), *SearchResult.GetSessionIdStr())
+			}
+		JoinSession();
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Find Sessions was not successful"))
+	}
+}
+
+void UMainGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type JoinResult)
+{
+	if (SessionInterface.IsValid())
+	{
+		FString Address;
+		SessionInterface->GetResolvedConnectString(SessionName, Address);
+
+		APlayerController* PlayerController = GetFirstLocalPlayerController();
+		if (PlayerController)
+		{
+			FInputModeGameOnly InputState;
+			GetFirstLocalPlayerController()->SetInputMode(InputState);
+			PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+		}
 	}
 }
